8798c97c3d126be4a5ea401f10f8abbafdce9844
gustavo veloso <gugaveloso12@gmail.com>
2026-07-14 12:22:07 -0300

    feat: implement jwt authentication

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,8 +1,12 @@
 import type { FastifyInstance } from 'fastify'
 import { register } from './controllers/register.js'
 import { authenticate } from './controllers/authenticate.js'
+import { profile } from './controllers/profile.js'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
+
+  /** Authenticated */
+  app.get('/me', profile)
 }
