a32de5ff4cfe544032498a582bd24bc658bc6d49
gustavo veloso <gugaveloso12@gmail.com>
2026-07-20 12:05:33 -0300

    fix: use haversine formula in findManyNearby to avoid acos domain error

--- src/repositories/prisma/prisma-gyms-repository.ts
+++ src/repositories/prisma/prisma-gyms-repository.ts
@@ -18,7 +18,11 @@ export class PrismaGymsRepository implements GymsRepository {
   async findManyNearby({ latitude, longitude }: FindManyNearbyParams) {
     const gyms = await prisma.$queryRaw<Gym[]>`
     SELECT * FROM gyms
-    WHERE ( 6371 * acos( cos( radians(${latitude}) ) * cos( radians( latitude ) ) * cos( radians( longitude ) - radians(${longitude}) ) + sin( radians(${latitude}) ) * sin( radians( latitude ) ) ) ) <= 10
+    WHERE ( 6371 * 2 * asin( sqrt(
+      power( sin( radians( (${latitude} - latitude) / 2 ) ), 2 )
+      + cos( radians(latitude) ) * cos( radians(${latitude}) )
+      * power( sin( radians( (${longitude} - longitude) / 2 ) ), 2 )
+    ) ) ) <= 10
     `
 
     return gyms
